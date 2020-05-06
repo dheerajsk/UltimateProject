490e140d5ef881e667824464c9bef170e3e8eb76
Dheeraj Kumar <kumardh@transitioncomputing.com>
2020-05-06 21:51:14 +0530

    Add project files.

---
+++ UltimateProject/Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace UltimateProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly IRepositoryManager _repository;
+
+        private ILoggerManager _logger;
+
+        public WeatherForecastController(ILoggerManager logger, IRepositoryManager repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            //_logger.LogInfo("Here is info message from our values controller.");
+            //_logger.LogDebug("Here is debug message from our values controller.");
+            //_logger.LogWarn("Here is warn message from our values controller.");
+            //_logger.LogError("Here is an error message from our values controller.");
+
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ UltimateProject/Extensions/ServiceExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using LoggerService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Entities;
+using Repository;
+
+namespace UltimateProject.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCORS(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddScoped<ILoggerManager, LoggerManager>();
+
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+            services.AddDbContext<RepositoryContext>(opts => opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+
+        public static void ConfigureRepositoryManager(this IServiceCollection services) =>
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+    }
+}
